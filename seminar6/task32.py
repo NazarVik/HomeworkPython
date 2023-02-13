4813f572323460a813de6e06ac074625b422a5e8
NazarVik <Nazarov_dev@icloud.com>
2023-02-13 22:30:05 +0300

    ready task 32

--- seminar6/task32.py
+++ seminar6/task32.py
@@ -2,7 +2,7 @@
 # значения которых принадлежат заданному диапазону
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 list1 = [7, 9, 17, 13, 15, 4, 3, 54, 23, 3, 2]
-minNum = int(input())
-maxNum = int(input())
+minNum = int(input("enter min number = "))
+maxNum = int(input("enter max number = "))
 
-print([i for i in range(len(list1)) if minNum <= list1[i] <= maxNum])
+print(*[i for i in range(len(list1)) if minNum <= list1[i] <= maxNum])
