9ddbc199faec5cca90e2592cc524f1c6008ed6a8
NazarVik <Nazarov_dev@icloud.com>
2023-02-13 23:51:01 +0300

    ready task 32

--- seminar6/task30.py
+++ seminar6/task30.py
@@ -2,9 +2,9 @@
 # нужно ввести с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
-firstElement, step, n = (map(int, input().split()))
-list1 = []
-
-for i in range(1, n + 1):
-    list1.append(firstElement + ((i-1) * step))
-print(list1)
+# firstElement, step, n = (map(int, input().split()))
+# list1 = []
+#
+# for i in range(1, n + 1):
+#     list1.append(firstElement + ((i-1) * step))
+# print(list1)
