6c51d1960e2d7363422bdb3f8c7405be0e157b61
NazarVik <Nazarov_dev@icloud.com>
2023-02-20 21:38:32 +0300

    ready task 36

--- seminar7/task36.py
+++ seminar7/task36.py
@@ -5,10 +5,10 @@
 # ровно два аргумента, как, например, у операции умножения.
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows + 1):
+    for i in range(1, num_rows + 1):   #
         for j in range(1, num_columns + 1):
             print(operation(i, j), end="\t")
         print()
 
 
-print_operation_table(lambda x, y: x * y, 4, 5)
+print_operation_table(lambda x, y: x * y, int(input("rows = ")), int(input("columns = ")))
