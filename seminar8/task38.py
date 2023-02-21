ebbcd978b19ce089c747d473cccee3828f032164
NazarVik <Nazarov_dev@icloud.com>
2023-02-21 20:02:40 +0300

    ready task 38

--- seminar8/task38.py
+++ seminar8/task38.py
@@ -13,20 +13,19 @@
 def input_data():
     with open("data.txt", "a") as f:
         for i in range(int(input("введите кол-во новых записей в справочник - "))):
-            f.write(input("введите ФИО и номер телефона: ").upper())
-            f.write("\n")
+            f.write(input("введите ФИО и номер телефона: " + "\n").upper())
 
 
 def read_data():
-    with open("data.txt", "r") as f:
+    with open("data.txt") as f:
         print(f.read())
 
 
 def find_data():
-    with open("data.txt", "r") as f:
-        find_feature = input("введите одну из характеристик для поиска - ").upper()
+    with open("data.txt") as f:
+        find_feature = input("введите одну из характеристик для поиска(фио или телефон - ").upper()
         flag = False
-        for i in f.readlines():
+        for i in f:
             list_feature = i.split()
             if find_feature in list_feature:
                 print(i, end="")
@@ -34,23 +33,37 @@ def find_data():
         if not flag:
             print("такой записи не найдено")
 
-def reload_data():
-    search_data = input("введите Имя или Фамилию для редактирования данных : ").upper()
-    change_data = input("введите новые данные : ").upper()
-    with open("data1.txt", "r+") as f:
-        for i in f.readlines():
-            list_feature = i.split()
-            if search_data in list_feature:
-                new_str = i.replace(search_data, change_data)
-                print(new_str)
-                f.write(new_str)
+
+def replace_data():
+    edit_data = input("введите Имя или Фамилию для редактирования данных : ").upper()
+    with open("data.txt") as f:
+        list_temp = []
+        for line in f:
+            if edit_data in line.split():
+                list_temp.append(input(f"{line} -->  ").upper() + "\n")
+            else:
+                list_temp.append(line)
+    with open("data.txt", "w") as f:
+        f.writelines(list_temp)
 
 
+def remove_data():
+    edit_data = input("введите Имя или Фамилию для удаления записи из справочника : ").upper()
+    with open("data.txt") as f:
+        list_temp = []
+        for line in f:
+            if edit_data in line.split():
+                print(f'{line} удалена ')
+                continue
+            else:
+                list_temp.append(line)
+    with open("data.txt", "w") as f:
+        f.writelines(list_temp)
 
 
-while 1:
+while True:
     get_choice = (int(input(
-        """введите 1 - для добавления записи,
+        """\n введите 1 - для добавления записи,
          2 - для вывода всего справочника, 
          3 - для поиска, 
          4 - для изменения данных
@@ -64,4 +77,8 @@ while 1:
         case 3:
             find_data()
         case 4:
-            reload_data()
+            replace_data()
+        case 5:
+            remove_data()
+        case _:
+            break
